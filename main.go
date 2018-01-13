e3fec7b69a7e0c15fd4b77ecc42df3cd276e206d
swfz <sawafuji.09@gmail.com>
2018-01-14 00:36:41 +0900

    create issue

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/google/go-github/github"
+	"github.com/k0kubun/pp"
+	"golang.org/x/oauth2"
+	"os"
+)
+
+type labelslice []string
+
+func (s *labelslice) String() string {
+	return fmt.Sprintf("%v", labelargs)
+}
+
+func (s *labelslice) Set(v string) error {
+	*s = append(*s, v)
+	return nil
+}
+
+var labelargs labelslice
+
+func main() {
+	var (
+		user  = flag.String("user", "", "string flag")
+		body  = flag.String("body", "", "string flag")
+		title = flag.String("title", "", "string flag")
+	)
+	flag.Var(&labelargs, "labels", "Data values.")
+	flag.Parse()
+
+	labels := make([]string, len(labelargs))
+	copy(labels, labelargs)
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+
+	client := github.NewClient(tc)
+	opt := &github.IssueRequest{
+		Title:    title,
+		Body:     body,
+		Assignee: user,
+		Labels:   &labels,
+	}
+
+	issue, _, err := client.Issues.Create(oauth2.NoContext, "swfz", "memo", opt)
+	// pp.Print(client.Issues)
+	pp.Print(issue)
+	pp.Print(err)
+}
